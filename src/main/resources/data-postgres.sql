cd7cf845276aa79d4bbb689c227e5786a67b83f6
Dijana <dijana.mukic@synechron.com>
2021-10-28 19:20:36 +0200

    data-postgres.sql added

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,10 @@
+/*USERS*/
+INSERT INTO users(first_name, last_name, email, password, residence, city, state, phone_number, user_type, enabled, user_role)
+VALUES ('Goca', 'Petrusic', 'goca@gmail.com', 'goca', 'Novi Sad', 'Novi Sad', 'Srbija', '0641234567', 'system_administrator', true, 0);
+
+INSERT INTO users(first_name, last_name, email, password, residence, city, state, phone_number, user_type, enabled, user_role)
+VALUES ('Vikendica', 'Petrusic', 'vikendica@gmail.com', 'vikendica', 'Novi Sad', 'Novi Sad', 'Srbija', '0641234567', 'cottage_owner', true, 1);
+
+INSERT INTO users(first_name, last_name, email, password, residence, city, state, phone_number, user_type, enabled, user_role)
+VALUES ('Brod', 'Petrusic', 'brod@gmail.com', 'brod', 'Novi Sad', 'Novi Sad', 'Srbija', '0641234567', 'boat_owner', true, 2);
+
